c3aa798962b98c4a86f109b8dd177c18480e1e7f
nobislinkdev <101082815+nobislinkdev@users.noreply.github.com>
2024-08-22 15:09:53 +0530

    add route

--- controller/bustInfoController.js
+++ controller/bustInfoController.js
@@ -1,14 +1,20 @@
 const Businfo = require("../models/busInfo");
+const routeInfo = require("../models/routeinfo");
 
 async function busDetails(req, res) {
   try {
     const { date, location, busNumber, price } = req.body;
+    const exsitRoute = await routeInfo.findById(req.params.id);
+    if (!exsitRoute) {
+      res.status(404).json(`Route not found`);
+    }
 
     const busdetails = await Businfo.create({
       date,
       location,
       busNumber,
       price,
+      route: exsitRoute._id,
     });
     res.status(200).json({ data: busdetails });
   } catch (error) {
@@ -18,12 +24,17 @@ async function busDetails(req, res) {
 async function busDetailsupdate(req, res) {
   try {
     const { date, location, busNumber, price } = req.body;
+    const exsitRoute = await routeInfo.findById(req.params.id);
+    if (!exsitRoute) {
+      res.status(404).json(`Route not found`);
+    }
 
     const busdetails = await Businfo.findByIdAndUpdate(req.params.id, {
       date,
       location,
       busNumber,
       price,
+      route: exsitRoute._id,
     });
     res.status(200).json({ data: busdetails });
   } catch (error) {
