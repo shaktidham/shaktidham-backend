a9fe79d3c6f5a751b7c45505cbaa83bcfa73226e
vinaysakariya <158840298+vinaysakariya@users.noreply.github.com>
2024-11-16 11:59:10 +0530

    n

--- seed.js
+++ seed.js
@@ -15,7 +15,7 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-// Define schemas
+// Define schemasg      
 
 async function seedAdminAndRole() {
   try {
