9266fcc2751dbfe3aab7ef7229bc19e887f10a59
raju <158840298+vinaysakariya@users.noreply.github.com>
2025-03-04 18:27:44 +0530

    add

--- routes/agent.js
+++ routes/agent.js
@@ -1,19 +1,22 @@
-const express = require('express');
-const { createAgent, getAgent, updateAgentById, deleteAgentById } = require('../controller/agentController');
+const express = require("express");
+const {
+  createAgent,
+  getAgent,
+  updateAgentById,
+  deleteAgentById,
+} = require("../controller/agentController");
 
 const router = express.Router();
 
-router.post('/agents', createAgent);
+router.post("/agents", createAgent);
 
-// Get Agent (by name)
-router.get('/agents', getAgent);
+// Get Agent (by name)sds
+router.get("/agents", getAgent);
 
 // Update Agent (by name)
-router.put('/agents/:id', updateAgentById);
+router.put("/agents/:id", updateAgentById);
 
 // Delete Agent (by name)
-router.delete('/agents/:id', deleteAgentById);
-
-
+router.delete("/agents/:id", deleteAgentById);
 
 module.exports = router;
